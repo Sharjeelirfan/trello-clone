8786bb0f21ff55cae915dfc75ee9db567a11d1d5
sharjeel <sharjeelkhatri303@gmail.com>
2024-06-28 15:08:10 +0500

    save in local storage

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const addcard = document.querySelector("#add-card")
+const cardinputform = document.querySelector("#card-input-form")
+const addcardbtn = document.querySelector("#add-card-btn")
+const addcardclose = document.querySelector("#card-input-close")
+const cardbtnandclose = document.querySelector("#form-btn-and-close")
+
+
 const createdcard = document.querySelector("#created-cards")
 
 
-addcard.addEventListener("keyup" , (event) =>{
-    if(event.key === "Enter"){
-    event.preventDefault()
-    let text = addcard.value
-    if(text.trim() === ""){
-        alert("Please enter text")
-
-    }else{
-        addnewcard(text)
-        addcard.value = ""
-    }
-}
-    // if(text !== ""){
-    //     addnewcard(text)
-    // }
+cardinputform.addEventListener("click" , (event) =>{
+    event.stopPropagation()
+    // addcardbtn.style.display = "inline"
+    // addcardclose.style.display = "inline"
+    cardbtnandclose.style.display = "block"
 })
+
+addcardclose.addEventListener("click" , (event) =>{
+        event.stopPropagation()
+        addcard.value = "";
+        cardbtnandclose.style.display = "none"
+        
+})
+document.addEventListener("click", (event) => {
+    if (!cardinputform.contains(event.target)) {
+        // addcardbtn.style.display = "none";
+        // addcardclose.style.display = "none";
+        cardbtnandclose.style.display = "none"
+    }
+});
 
 function addnewcard(text){
     let addedcards = document.createElement("div")
@@ -26,4 +36,41 @@ function addnewcard(text){
     addedcards.innerHTML = text
 
     createdcard.appendChild(addedcards)
+    storecard(text)
 }
+
+function storecard(text){
+    let cards = JSON.parse(localStorage.getItem("cards")) || []
+    cards.push(text)
+    localStorage.setItem('cards' , JSON.stringify(cards))
+}
+
+
+
+function loadcards(){
+    let cards = JSON.parse(localStorage.getItem("cards")) || []
+    cards.forEach((text) => {
+        addnewcard(text)
+        
+    });
+}
+
+cardinputform.addEventListener("submit" , (event) =>{
+    event.preventDefault()
+    let text = addcard.value
+    if(text.trim() === ""){
+        alert("Please enter text")
+    }else{
+        addnewcard(text)
+        addcard.value = ""
+    }
+    // if(text !== ""){
+        //     addnewcard(text)
+        // }
+        
+    })
+
+
+    document.addEventListener("DOMContentLoaded" , () =>{
+        loadcards()
+    })
